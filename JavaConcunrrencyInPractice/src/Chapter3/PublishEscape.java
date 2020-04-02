46b6d739662f28c238db9d94a841003d398b889d
amitg <amitg@LAPTOP-CFJVV32J>
2020-04-02 17:24:19 +0530

    Chapter 3 Practice Code

---
+++ JavaConcunrrencyInPractice/src/Chapter3/PublishEscape.java
@@ -0,0 +1,22 @@
+package Chapter3;
+
+public class PublishEscape {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+interface EventListener{
+	void onEvent(Event e);
+}
+
+interface Event{
+	
+}
+
+class SafeListener{
+	
+}
--- JavaConcunrrencyInPractice/src/Chapter3/SharedVariablesWithOutSynchronization.java
+++ JavaConcunrrencyInPractice/src/Chapter3/SharedVariablesWithOutSynchronization.java
@@ -3,21 +3,20 @@ package Chapter3;
 public class SharedVariablesWithOutSynchronization {
 	private static boolean ready;
 	private static int number;
-	
-	private static class ReaderThread extends Thread{
+
+	private static class ReaderThread extends Thread {
 		public void run() {
-			while(!ready)
+			while (!ready)
 				Thread.yield();
 			System.out.println(number);
 		}
 	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-     new ReaderThread().start();
-      number=42;
-      ready=true;
+		new ReaderThread().start();
+		number = 42;
+		ready = true;
 	}
 
 }
-
-
