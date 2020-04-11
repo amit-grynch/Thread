1e194df083bb75fac1a4894b9a0e7b661ca428cc
amitg <amitg@LAPTOP-CFJVV32J>
2020-04-11 11:26:01 +0530

    Producer-Consumer Using Wait-Notify Approach

---
+++ JavaConcunrrencyInPractice/src/Chapter5/ProducerConsumerProblemUsingWaitNotifyApproach.java
@@ -0,0 +1,82 @@
+package Chapter5;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
+
+public class ProducerConsumerProblemUsingWaitNotifyApproach {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Queue<Integer> queue=new LinkedList<Integer>();
+			new CustomCounsumerThread(queue, 10).start();;
+			new CustomProducerThread(queue, 5).start();;
+			
+		}
+
+	}
+
+
+class CustomProducerThread extends Thread{
+	private Queue<Integer> queue;
+	private int size;
+	int i;
+	public CustomProducerThread(Queue<Integer> queue,int size) {
+		this.queue=queue;
+		this.size=size;
+	}
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		while(true) {
+			synchronized(queue) {
+				while(queue.size()==size) {
+					try {
+						System.out.println("Queue is full and waiting the Consumer to consume :");
+						queue.wait();
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				Random randomInt=new Random();
+				i=randomInt.nextInt(10);
+				System.out.println("Producer is Adding Number in Queue : "+i);
+				queue.add(i);
+			    queue.notifyAll();	
+			}
+		}
+	}
+}
+
+class CustomCounsumerThread extends Thread{
+	int size;
+	int i;
+	Queue<Integer> queue;
+	CustomCounsumerThread(Queue<Integer> queue,int size){
+		this.queue=queue;
+		this.size=size;	
+	}
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		while(true) {
+			synchronized (queue) {
+				 while(queue.size()==0) {
+					 System.out.println("Queue is Empty and waiting the Producer  to Produce :");
+					 try {
+						queue.wait();
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				 }
+				 
+				 i=queue.remove();
+				 System.out.println("Consumer Has Removed The Number From Queue :"+i);
+				 queue.notifyAll();
+			}
+		}
+	}
+}
