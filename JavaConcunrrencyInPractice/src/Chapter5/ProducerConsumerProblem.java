b1504f5713ae0e8268cc14ea6d7ade3e08f5bf63
amitg <amitg@LAPTOP-CFJVV32J>
2020-04-11 09:40:06 +0530

    Consumer Producer Problem Code

--- JavaConcunrrencyInPractice/src/Chapter5/ProducerConsumerProblem.java
+++ JavaConcunrrencyInPractice/src/Chapter5/ProducerConsumerProblem.java
@@ -1,10 +1,65 @@
 package Chapter5;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
 public class ProducerConsumerProblem {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		BlockingQueue<Integer> queue=new ArrayBlockingQueue<Integer>(20);
+		new Thread(new ProducerThread(queue)).start();
+		new Thread(new ConsumerThread(queue)).start();
 
 	}
 
+}
+
+
+class ProducerThread implements Runnable{
+	
+	private final BlockingQueue<Integer> queue;
+    public ProducerThread(BlockingQueue<Integer> queue) {
+    	this.queue=queue;
+    }
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		for(int index=0;index<10;index++) {
+			try {
+				System.out.println("Producer is putting Value  : "+index);
+				queue.put(index);
+				System.out.println("Current Capacity After Putting Value In Queue By producer :"+queue.size());
+				Thread.sleep(100);
+				
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	
+}
+
+class ConsumerThread implements Runnable{
+	private final BlockingQueue<Integer> queue;
+	public ConsumerThread(BlockingQueue<Integer> queue) {
+		this.queue=queue;
+	}
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		try {
+			while(true) {
+			System.out.println("Consumer is taking Value : "+queue.take());
+			System.out.println("Current Capacity After Taking Value From Queue By Consumer :"+queue.size());
+			Thread.sleep(1000);
+			}
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 }
