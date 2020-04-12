ff4aef3312202c2ae827a9d9f71be81ba843564a
amitg <amitg@LAPTOP-CFJVV32J>
2020-04-13 00:20:16 +0530

    Formating the Code Structure

--- JavaConcunrrencyInPractice/src/Chapter5/CyclicBarrierExample.java
+++ JavaConcunrrencyInPractice/src/Chapter5/CyclicBarrierExample.java
@@ -7,51 +7,52 @@ public class CyclicBarrierExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Runnable barrierAction1=new Runnable() {
-		  // This is the task which take place after reaching Barrier Point
+		Runnable barrierAction1 = new Runnable() {
+			// This is the task which take place after reaching Barrier Point
 			public void run() {
 				System.out.println("Barrier Action 1");
 			};
 		};
-		Runnable barrierAction2=new Runnable() {
+		Runnable barrierAction2 = new Runnable() {
 			public void run() {
 				System.out.println("Barrier Action 2");
 			};
 		};
-		 CyclicBarrier cb1=new CyclicBarrier(2,barrierAction1);
-		CyclicBarrier cb2=new CyclicBarrier(2,barrierAction2);
-		 new Thread(new CyclicBarrierRunnanle(cb1,cb2)).start();
-		 new Thread(new CyclicBarrierRunnanle(cb1,cb2)).start();
-		 
+		CyclicBarrier cb1 = new CyclicBarrier(2, barrierAction1);
+		CyclicBarrier cb2 = new CyclicBarrier(2, barrierAction2);
+		new Thread(new CyclicBarrierRunnanle(cb1, cb2)).start();
+		new Thread(new CyclicBarrierRunnanle(cb1, cb2)).start();
 
 	}
 
 }
 
-class CyclicBarrierRunnanle implements Runnable{
+class CyclicBarrierRunnanle implements Runnable {
 	CyclicBarrier barrier1;
 	CyclicBarrier barrier2;
-	CyclicBarrierRunnanle(CyclicBarrier barrier1,CyclicBarrier barrier2){
-		this.barrier1=barrier1;
-		this.barrier2=barrier2;
-	
+
+	CyclicBarrierRunnanle(CyclicBarrier barrier1, CyclicBarrier barrier2) {
+		this.barrier1 = barrier1;
+		this.barrier2 = barrier2;
+
 	}
+
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		
+
 		try {
-			System.out.println(" Thread : "+Thread.currentThread().getName()+" waiting Barrier1");
+			System.out.println(" Thread : " + Thread.currentThread().getName() + " waiting Barrier1");
 			barrier1.await();
 			Thread.sleep(1000);
-			System.out.println(" Thread : "+Thread.currentThread().getName()+" waiting Barrier2");
+			System.out.println(" Thread : " + Thread.currentThread().getName() + " waiting Barrier2");
 			barrier2.await();
-			System.out.println("Thread "+Thread.currentThread().getName()+" Done");
+			System.out.println("Thread " + Thread.currentThread().getName() + " Done");
 		} catch (InterruptedException | BrokenBarrierException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
 }
