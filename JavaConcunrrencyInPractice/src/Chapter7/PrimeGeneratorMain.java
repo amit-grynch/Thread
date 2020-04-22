db934f5ad1f8dc5eecc26dc12e6d7c1f13b85d13
amitg <amitg@LAPTOP-CFJVV32J>
2020-04-22 10:27:17 +0530

    Formatted The Code

--- JavaConcunrrencyInPractice/src/Chapter7/PrimeGeneratorMain.java
+++ JavaConcunrrencyInPractice/src/Chapter7/PrimeGeneratorMain.java
@@ -8,51 +8,53 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 /**
- * @author amitg
- *  Demonstrate About How Thread/Task can be Cancelled up on Cancellation Request
+ * @author amitg Demonstrate About How Thread/Task can be Cancelled up on
+ *         Cancellation Request
  */
 public class PrimeGeneratorMain {
-  
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		 PrimeGenerator primeGenerator=new PrimeGenerator();
-		 ExecutorService executor =Executors.newFixedThreadPool(2);
-		 executor.submit(primeGenerator);
-		 try {
-			 Thread.sleep(1000);
-		 } catch (InterruptedException e) {
+		PrimeGenerator primeGenerator = new PrimeGenerator();
+		ExecutorService executor = Executors.newFixedThreadPool(2);
+		executor.submit(primeGenerator);
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}finally {
+		} finally {
 			primeGenerator.cancel();
 		}
-        System.out.println(primeGenerator.get());   
+		System.out.println(primeGenerator.get());
 	}
 
 }
 
-class PrimeGenerator implements Runnable{
-   private final List<BigInteger> primeNumberList=new ArrayList<BigInteger>();
-   private volatile boolean cancellationRequest;
+class PrimeGenerator implements Runnable {
+	private final List<BigInteger> primeNumberList = new ArrayList<BigInteger>();
+	private volatile boolean cancellationRequest;
+
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		BigInteger p=BigInteger.ONE;
-		while(!cancellationRequest) {
-			p=p.nextProbablePrime();
+		BigInteger p = BigInteger.ONE;
+		while (!cancellationRequest) {
+			p = p.nextProbablePrime();
 			synchronized (p) {
 				primeNumberList.add(p);
 			}
 		}
-		 if(cancellationRequest) {
-			 System.out.println("Cancellation Request Came");
-		 }
+		if (cancellationRequest) {
+			System.out.println("Cancellation Request Came");
+		}
 	}
-	
+
 	public void cancel() {
-		cancellationRequest=true;
+		cancellationRequest = true;
 	}
-	public synchronized List<BigInteger> get(){
-		 return new ArrayList<BigInteger>(primeNumberList);
+
+	public synchronized List<BigInteger> get() {
+		return new ArrayList<BigInteger>(primeNumberList);
 	}
 }
--- JavaConcunrrencyInPractice/src/Chapter7/UnrelaibleCancellationTask.java
+++ JavaConcunrrencyInPractice/src/Chapter7/UnrelaibleCancellationTask.java
@@ -6,30 +6,29 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-
 /**
- * @author amitg
- *  Code is unreliable if We call cancel method because put is block when queue is full.
+ * @author amitg Code is unreliable if We call cancel method because put is
+ *         block when queue is full.
  */
 public class UnrelaibleCancellationTask {
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ExecutorService executor=Executors.newFixedThreadPool(2);
-		BlockingQueue<BigInteger> primeBlockingQueue=new ArrayBlockingQueue<>(10);
-		BlockingQueuePrimeProducer producer=new BlockingQueuePrimeProducer(primeBlockingQueue);
-		BlockingQueuePrimeConsumer consumer=new BlockingQueuePrimeConsumer(primeBlockingQueue);
+		ExecutorService executor = Executors.newFixedThreadPool(2);
+		BlockingQueue<BigInteger> primeBlockingQueue = new ArrayBlockingQueue<>(10);
+		BlockingQueuePrimeProducer producer = new BlockingQueuePrimeProducer(primeBlockingQueue);
+		BlockingQueuePrimeConsumer consumer = new BlockingQueuePrimeConsumer(primeBlockingQueue);
 		executor.submit(producer);
 		executor.submit(consumer);
 		try {
-		while(primeBlockingQueue.size()>0) {
-			consumer.run();
-		}
-		}finally {
+			while (primeBlockingQueue.size() > 0) {
+				consumer.run();
+			}
+		} finally {
 			producer.cancel();
 		}
 	}
-   
+
 }
 
 class BlockingQueuePrimeProducer implements Runnable {
@@ -47,7 +46,7 @@ class BlockingQueuePrimeProducer implements Runnable {
 		while (!cancellationRequest) {
 			try {
 				primeQueue.put(primeNumber.nextProbablePrime());
-				System.out.println("Produced PrimeNumber = "+primeNumber);
+				System.out.println("Produced PrimeNumber = " + primeNumber);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -61,25 +60,26 @@ class BlockingQueuePrimeProducer implements Runnable {
 	}
 }
 
-class BlockingQueuePrimeConsumer implements Runnable{
+class BlockingQueuePrimeConsumer implements Runnable {
 	private final BlockingQueue<BigInteger> primeConsumerQueue;
-	BlockingQueuePrimeConsumer(BlockingQueue<BigInteger> primeConsumerQueue){
-		this.primeConsumerQueue=primeConsumerQueue;
+
+	BlockingQueuePrimeConsumer(BlockingQueue<BigInteger> primeConsumerQueue) {
+		this.primeConsumerQueue = primeConsumerQueue;
 	}
+
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
 		try {
-			BigInteger intConsumer=primeConsumerQueue.take();
-			System.out.println("Counsumed Prime NUmber is ="+intConsumer);
+			BigInteger intConsumer = primeConsumerQueue.take();
+			System.out.println("Counsumed Prime NUmber is =" + intConsumer);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} finally {
+
 		}
-		finally {
-			
-		}
-		
+
 	}
-	
+
 }
