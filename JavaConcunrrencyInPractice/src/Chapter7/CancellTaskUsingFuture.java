b796b228b7cf27843bd643152c6934886407117c
amitg <amitg@LAPTOP-CFJVV32J>
2020-04-24 10:33:15 +0530

    Cancel a task using Future's Cancel and Formatted the Code

--- JavaConcunrrencyInPractice/src/Chapter7/CancellTaskUsingFuture.java
+++ JavaConcunrrencyInPractice/src/Chapter7/CancellTaskUsingFuture.java
@@ -8,16 +8,18 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
 public class CancellTaskUsingFuture {
-	 private final static ExecutorService executor=Executors.newFixedThreadPool(1);
-	public void timedRun(Runnable r, long timeout,TimeUnit unit) throws InterruptedException {
-		  Future<?> task=executor.submit(r);
-		    try {
-				task.get(timeout,unit);
-			} catch (InterruptedException | ExecutionException | TimeoutException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}finally {
-				task.cancel(true);			}
+	private final static ExecutorService executor = Executors.newFixedThreadPool(1);
+
+	public void timedRun(Runnable r, long timeout, TimeUnit unit) throws InterruptedException {
+		Future<?> task = executor.submit(r);
+		try {
+			task.get(timeout, unit);
+		} catch (InterruptedException | ExecutionException | TimeoutException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			task.cancel(true);
+		}
 	}
 
 }
