3d6b42745f333490f4ff8007fefdd00d8d2a1960
amitg <amitg@host.docker.internal>
2021-05-19 16:34:37 +0530

    Thread Implementation

---
+++ Efficient Java Multithreading and Concurrency with Executors/src/com/threadCreation/FirstThreadCreationTechnique.java
@@ -0,0 +1,35 @@
+package com.threadCreation;
+
+import java.util.concurrent.TimeUnit;
+
+public class FirstThreadCreationTechnique {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" Before Custom Thread Creation ");
+      new ThreadExample();
+      System.out.println(" After Custom Thread Creation ");
+	}
+
+}
+
+class ThreadExample extends Thread{
+	@Override
+	public void run() {
+
+  for(int i=0;i<10;i++) {
+	  System.out.println(" First Thread creation Technique--"+i);
+  }
+  
+  try {
+	TimeUnit.MICROSECONDS.sleep(250);
+} catch (InterruptedException e) {
+	// TODO Auto-generated catch block
+	e.printStackTrace();
+}
+	}
+	
+	public ThreadExample() {
+		this.start();
+	}
+}
