40f866f6d28fde803e9aeb7337ee825bd75bf02d
amitg <amitg@host.docker.internal>
2021-05-19 16:45:08 +0530

    Thread Creation and Refactot

---
+++ Efficient Java Multithreading and Concurrency with Executors/src/com/threadCreation/SecondThreadCreationTechnique.java
@@ -0,0 +1,27 @@
+package com.threadCreation;
+
+public class SecondThreadCreationTechnique {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new SceondThread().start();
+	}
+
+}
+
+class SceondThread extends Thread {
+	int count = 0;
+	int id = 0;
+
+	public void run() {
+
+		for (int i = 10; i > 0; i--) {
+			System.out.println(" Second Thread Created " + i);
+		}
+	}
+
+	public SceondThread() {
+		// TODO Auto-generated constructor stub
+		id = ++count;
+	}
+}
