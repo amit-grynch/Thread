0f7ff243e2257243e04b1d8c2e856b589301b2f5
amit-grynch <amit.grynch@gmail.com>
2021-05-30 19:44:14 +0530

    Using Interrupt Method To Terminate Task

---
+++ Efficient Java Multithreading and Concurrency with Executors/src/com/TerminatingThread/TerminatingThreadsSecondTechnique.java
@@ -0,0 +1,37 @@
+package com.TerminatingThread;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class TerminatingThreadsSecondTechnique {
+
+	public static void main(String[] args) throws InterruptedException {
+		String currentThreadName = Thread.currentThread().getName();
+		System.out.println("[" + currentThreadName + "] Main thread starts here...");
+		
+		Thread t1 = new Thread(new LoopTaskF(), "MyThread-1");
+		t1.start();
+		
+		Thread t2 = new Thread(new LoopTaskF(), "MyThread-2");
+		t2.start();
+		
+		Thread t3 = new Thread(new LoopTaskF(), "MyThread-3");
+		t3.start();
+		
+		TimeUnit.MILLISECONDS.sleep(3000);
+		
+		System.out.println("[" + currentThreadName + "] Interrupting " + t1.getName() + "....");
+		t1.interrupt();
+		System.out.println("[" + currentThreadName + "] Interrupting Status : " + t1.isInterrupted() + "....");
+		
+		System.out.println("[" + currentThreadName + "] Interrupting " + t2.getName() + "....");
+		t2.interrupt();
+		System.out.println("[" + currentThreadName + "] Interrupting Status : " + t2.isInterrupted() + "....");
+		
+		System.out.println("[" + currentThreadName + "] Interrupting " + t3.getName() + "....");
+		t3.interrupt();
+		System.out.println("[" + currentThreadName + "] Interrupting Status : " + t3.isInterrupted() + "....");
+		
+		System.out.println("[" + currentThreadName + "] Main thread ends here...");
+	}	
+}
