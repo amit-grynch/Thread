c07662f1b9e11ee2c24e52b177f2170705b3c97a
amit-grynch <amit.grynch@gmail.com>
2021-05-31 18:09:14 +0530

    Blocking Executors Task Termination

---
+++ Efficient Java Multithreading and Concurrency with Executors/src/com/TerminatingThread/LoopTaskA.java
@@ -0,0 +1,32 @@
+package com.TerminatingThread;
+
+import java.util.concurrent.TimeUnit;
+
+public class LoopTaskA implements Runnable {
+	
+	private static int count = 0;
+	private int id;
+	
+	@Override
+	public void run() {
+		System.out.println("##### <TASK-" + id + "> STARTING #####");
+		
+		for (int i=10; i>0; i--) {
+			System.out.println("<" + id + ">TICK TICK - " + i);
+			
+			try {
+				TimeUnit.MILLISECONDS.sleep((long)(Math.random() * 1000));
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				break;
+			}
+		}
+		
+		System.out.println("***** <TASK-" + id + "> COMPLETED *****");
+	}
+	
+	public LoopTaskA() {
+		this.id = ++count;
+	}
+}
